d389cda46f5422657408a99b02924dd52ca03496
eslamelhefny <32838993+eslamelhefny@users.noreply.github.com>
2021-02-25 23:47:31 +0200

    Add files via upload

---
+++ BIT_MATH.h
@@ -0,0 +1,14 @@
+/************************************************************************/
+/*	AUTHOR		:		AHMED ATTIA ATTIA 								*/
+/*	VERSION		:		V01												*/
+/*	DATE		:		10	NOV	2020									*/
+/************************************************************************/
+
+#ifndef			BIT_MATH_H
+#define			BIT_MATH_H
+
+#define			SET_BIT(REG,BIT_NUM)		(REG |=  (1<<BIT_NUM))
+#define			CLR_BIT(REG,BIT_NUM)		(REG &= ~(1<<BIT_NUM))
+#define			GET_BIT(REG,BIT_NUM)		( (REG>>BIT_NUM)  & 1)
+
+#endif
